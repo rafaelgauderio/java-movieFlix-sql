b16a459456a4e99818ae80b083f4a8a9244a7031
Rafael de Luca <deluca1712@gmail.com>
2023-01-18 16:13:31 -0300

    Metodo GET Genre findById

--- src/main/java/com/rafaeldeluca/movieflix/resources/GenreResource.java
+++ src/main/java/com/rafaeldeluca/movieflix/resources/GenreResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,17 +13,23 @@ import com.rafaeldeluca.movieflix.services.GenreService;
 import com.rafaeldeluca.movifliex.dto.GenreDTO;
 
 @RestController
-@RequestMapping(value= "/genres")
+@RequestMapping(value = "/genres")
 public class GenreResource {
-	
+
 	@Autowired
 	private GenreService service;
 
 	@GetMapping
-	public ResponseEntity<List <GenreDTO>> findAll () {
-		
+	public ResponseEntity<List<GenreDTO>> findAll() { // RequestParam é um tipo de dado Opcional
+
 		List<GenreDTO> listaDTO = service.findAll();
 		return ResponseEntity.ok().body(listaDTO);
-		
+
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<GenreDTO> findById(@PathVariable Long id) { // PathVariable é um tipo de dado obrigatório
+		GenreDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 }
--- src/main/java/com/rafaeldeluca/movieflix/services/GenreService.java
+++ src/main/java/com/rafaeldeluca/movieflix/services/GenreService.java
@@ -1,6 +1,7 @@
 package com.rafaeldeluca.movieflix.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.rafaeldeluca.movieflix.entities.Genre;
 import com.rafaeldeluca.movieflix.repositories.GenreRepository;
+import com.rafaeldeluca.movieflix.services.exceptions.ResourceNotFoundException;
 import com.rafaeldeluca.movifliex.dto.GenreDTO;
 
 @Service
@@ -22,6 +24,15 @@ public class GenreService {
 		List<Genre> lista = repository.findAll();
 		return lista.stream().map(x -> new GenreDTO(x)).collect(Collectors.toList());
 		
-	}	
+	}
+	
+	@Transactional(readOnly=true)
+	public GenreDTO findById(Long id) {
+		Optional<Genre> objeto = repository.findById(id);
+		Genre entity = objeto.orElseThrow(() -> new ResourceNotFoundException("Genero não encontrado com o id informado: " + id));
+		GenreDTO dto = new GenreDTO(entity);
+		return dto;	
+		
+	}
 
 }
